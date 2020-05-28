6e9f412fbfe01a7e747060936a5055018bfef188
swond03 <swond03@users.noreply.github.com>
2020-05-28 01:07:08 +0100

    Initial commit

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MSYTASP.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MSYTASP.Model;
+
+namespace MSYTASP.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public JsonFileProductService _productService;
+        public ProductsController(JsonFileProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return _productService.GetProducts();
+        }
+
+        [Route("Rate")]
+        [HttpGet]
+        public ActionResult Patch(string productId, int rating)
+        {
+            _productService.AddRating(productId, rating);
+            return Ok();
+        }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MSYTASP.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using MSYTASP.Model;
+
+namespace MSYTASP.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly JsonFileProductService _productService;
+
+        public IEnumerable<Product> Products;
+
+
+        public IndexModel(ILogger<IndexModel> logger, JsonFileProductService productService)
+        {
+            _logger = logger;
+            _productService = productService;
+        }
+
+        public void OnGet()
+        {
+            Products = _productService.GetProducts();
+        }
+    }
+}
